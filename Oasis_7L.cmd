9970da861fad578d5e0d3bfa06fd6277b1d425db
Kiaaina JA Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-03-09 11:38:42 -1000

    Add files via upload

--- Oasis_7L.cmd
+++ Oasis_7L.cmd
@@ -7,12 +7,14 @@ title "Oasis_By_Kia7L"
         
 @set "rom=815623194318"
 @set "apk=917814321891"
+@set "app=192718563721"
 
-cecho {04}1=Oasis.rom{/h}2=Oasis.apk{07}
+cecho {04}1=Oasis.rom{/h}2=Oasis.apk{/h}3=Oasis.app{07}
 
 set /a m=Choose;
 if (%m%=1) & goto one
 if (%m%=2) & goto two
+if (%m%=3) & goto three
 
 goto top
 
@@ -23,6 +25,10 @@ goto 7L
 :two
 copy "%apk% ./Oasis.apk"
 goto 7L
+
+:three
+copy "%app% ./Oasis.app"
+got 7L
 
 :7L
 cecho {05}Made_By_Kia7L{02}
